153f3f34cb550893ec08753991126fd0c3101890
sett1n <92488233+sett1n@users.noreply.github.com>
2023-03-03 13:17:58 +0300

    Class_8
    Class_8

---
+++ Class_8/Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Введите длину слова:");
+int length = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите текст:");
+string text = Console.ReadLine();
+
+string pattern = $@"\b\w{{{length}}}\b";
+Regex regex = new Regex(pattern);
+
+Console.WriteLine($"Слова длины {length}:");
+foreach (Match match in regex.Matches(text))
+{
+    Console.WriteLine(match.Value);
+}
---
+++ Class_8/Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Введите слово для поиска:");
+string word = Console.ReadLine();
+
+Console.WriteLine("Введите текст:");
+string text = Console.ReadLine();
+
+string pattern = $@"\b{word}\b";
+Regex regex = new Regex(pattern);
+
+int count = regex.Matches(text).Count;
+if (count > 0)
+{
+    Console.WriteLine($"Слово '{word}' встречается в тексте {count} раз(а).");
+}
+else
+{
+    Console.WriteLine($"Слово '{word}' не найдено в тексте.");
+}
