686f4c2819c58d577dcf877c757e37304a04efb0
sett1n <92488233+sett1n@users.noreply.github.com>
2023-03-09 11:05:42 +0300

    Class_13
    Class_13

---
+++ Class_13/Task_2/Program.cs
@@ -0,0 +1,69 @@
+﻿delegate double ArithmeticOperation(double a, double b);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArithmeticOperation add = (a, b) => a + b;
+        ArithmeticOperation sub = (a, b) => a - b;
+        ArithmeticOperation mul = (a, b) => a * b;
+        ArithmeticOperation div = (a, b) =>
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Деление на ноль!");
+            }
+            return a / b;
+        };
+
+        Console.Write("Введите первое число: ");
+        double x;
+        if (!double.TryParse(Console.ReadLine(), out x))
+        {
+            Console.WriteLine("Некорректное число!");
+            return;
+        }
+
+        Console.Write("Введите второе число: ");
+        double y;
+        if (!double.TryParse(Console.ReadLine(), out y))
+        {
+            Console.WriteLine("Некорректное число!");
+            return;
+        }
+
+        Console.Write("Введите операцию (+, -, *, /): ");
+        string op = Console.ReadLine();
+
+        double result;
+
+        switch (op)
+        {
+            case "+":
+                result = add(x, y);
+                break;
+            case "-":
+                result = sub(x, y);
+                break;
+            case "*":
+                result = mul(x, y);
+                break;
+            case "/":
+                try
+                {
+                    result = div(x, y);
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+                break;
+            default:
+                Console.WriteLine("Некорректная операция!");
+                return;
+        }
+
+        Console.WriteLine("Результат: " + result);
+    }
+}
---
+++ Class_13/Task_3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = Console.ReadLine();
+
+        StringOperation operation;
+
+        operation = RemoveSpaces;
+        operation += Reverse;
+        operation += ToUpperCase;
+
+        operation(str);
+
+        Console.ReadKey();
+    }
+
+    static void RemoveSpaces(string str)
+    {
+        string result = str.Replace(" ", "");
+        Console.WriteLine("Строка без пробелов: " + result);
+    }
+
+    static void Reverse(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        string result = new string(charArray);
+        Console.WriteLine("Перевернутая строка: " + result);
+    }
+
+    static void ToUpperCase(string str)
+    {
+        string result = str.ToUpper();
+        Console.WriteLine("Строка в верхнем регистре: " + result);
+    }
+    delegate void StringOperation(string str);
+}
