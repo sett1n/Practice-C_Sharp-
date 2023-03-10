6d4d4b69d156615474b140b1102d820c3267df45
sett1n <92488233+sett1n@users.noreply.github.com>
2023-03-10 10:06:43 +0300

    Class_14
    Class_14

---
+++ Class_14/Task_1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+delegate void CalculationDelegate();
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CalculationDelegate delegateInstance = new CalculationDelegate(CalculateSquare);
+        delegateInstance += CalculateTriangle;
+
+        try
+        {
+            UseDelegate(delegateInstance);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Произошла ошибка: {e.Message}");
+        }
+    }
+
+    //2-ое задание
+
+    static void UseDelegate(CalculationDelegate del)
+    {
+        del();
+    }
+
+    //2-ое задание
+
+    static void CalculateSquare()
+    {
+        Console.WriteLine("Введите длину стороны квадрата:");
+
+        double sideLength = double.Parse(Console.ReadLine());
+
+        if (sideLength <= 0)
+        {
+            throw new ArgumentException("Длина стороны должна быть больше 0.");
+        }
+
+        double perimeter = 4 * sideLength;
+        double area = sideLength * sideLength;
+
+        Console.WriteLine($"Периметр: {perimeter}, Площадь: {area}");
+    }
+
+    static void CalculateTriangle()
+    {
+        Console.WriteLine("Введите длину стороны треугольника:");
+
+        double sideLength = double.Parse(Console.ReadLine());
+
+        if (sideLength <= 0)
+        {
+            throw new ArgumentException("Длина стороны должна быть больше 0.");
+        }
+
+        Console.WriteLine($"Длина стороны: {sideLength}");
+    }
+}
---
+++ Class_14/Task_3/Model/MyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3.Model
+{
+    internal class MyInfo
+    {
+        delegate void ChangingInformation();
+        event ChangingInformation ciEvent;
+
+
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    ciEvent?.Invoke();
+                }
+            }
+        }
+
+        public MyInfo(string name)
+        {
+            _name = name;
+            ciEvent += delegate ()
+            {
+                Console.WriteLine("Имя изменилось");
+            };
+        }
+    }
+}
