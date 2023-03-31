e183c8932ad3d2cdccb828f23cc79b8e1c4fd0be
sett1n <92488233+sett1n@users.noreply.github.com>
2023-03-31 19:25:46 +0300

    Class_26
    Class_26

---
+++ Class_26/Task_2/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox1_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (TextBox1.Text == "Введите имя")
+            {
+                TextBox1.Text = "";
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            LabelName.Text = "Hello " + TextBox1.Text;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Введите свое имя и нажмите на кнопку'Сказать Имя'");
+        }
+    }
+}
---
+++ Class_26/Task_3/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace Task_3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double x, y;
+            double h = 0.1;
+            List<KeyValuePair<double, double>> data = new List<KeyValuePair<double, double>>();
+
+            if (!Double.TryParse(textBox.Text, out x))
+            {
+                MessageBox.Show("Введите корректное значение X");
+                return;
+            }
+
+            while (x < Math.PI / 2)
+            {
+                y = Math.Tan(x);
+                data.Add(new KeyValuePair<double, double>(x, y));
+                x += h;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("X\tY");
+            foreach (KeyValuePair<double, double> pair in data)
+            {
+                sb.AppendLine(pair.Key.ToString() + "\t" + pair.Value.ToString());
+            }
+            textBlock.Text = sb.ToString();
+
+            chart.Series.Clear();
+            chart.Series.Add(new LineSeries
+            {
+                ItemsSource = data,
+                DependentValuePath = "Value",
+                IndependentValuePath = "Key"
+            });
+        }
+    }
+}
