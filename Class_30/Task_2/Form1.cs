3b5b654657fe1dd87b2313f7ad1969d0068b4b1b
sett1n <92488233+sett1n@users.noreply.github.com>
2023-04-01 20:50:09 +0300

    Class_30
    Class_30

---
+++ Class_30/Task_2/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_2
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Bitmap baner;
+        Rectangle rct;
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                baner = new Bitmap("baner.png");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки файла баннера\n {ex.ToString()} Баннер");
+                this.Close();
+                return;
+            }
+
+            rct.X = 0;
+            rct.Y = 0;
+            rct.Width = baner.Width;
+            rct.Height = baner.Height;
+
+            timer1.Interval = 50;
+            timer1.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Y < rct.Y + rct.Height) && (e.Y > rct.Y))
+            {
+                if (timer1.Enabled != false)
+                    timer1.Enabled = false;
+            }
+            else
+            {
+                if (timer1.Enabled != true)
+                    timer1.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            rct.X -= 1;
+            if (Math.Abs(rct.X) > rct.Width)
+                rct.X += rct.Width;
+            for (int i = 0; i < Convert.ToInt16(this.ClientSize.Width / rct.Width) + 1; i++)
+            {
+                g.DrawImage(baner, rct.X + i * rct.Width, rct.Y);
+            }
+        }
+    }
+}
---
+++ Class_30/Task_4/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_4
+{
+    public partial class Form1 : Form
+    {
+        Bitmap sky, plane;
+        Graphics g;
+        int dx;
+        Rectangle rct;
+        Boolean demo = true;
+        Random rnd;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            g.DrawImage(sky, new Point(0, 0));
+
+            if (rct.X < this.ClientRectangle.Width)
+                rct.X += dx;
+            else
+            {
+                rct.X = -40;
+                rct.Y = 20 +
+                    rnd.Next(this.ClientSize.Height - 40 - plane.Height);
+
+                dx = 2 + rnd.Next(4);
+            }
+
+            g.DrawImage(plane, rct.X, rct.Y);
+
+            if (!demo)
+                this.Invalidate(rct);
+            else
+            {
+                Rectangle reg = new Rectangle(20, 20, sky.Width - 40, sky.Height - 40);
+
+                g.DrawRectangle(Pens.Black, reg.X, reg.Y, reg.Width - 1, reg.Height - 1);
+
+                this.Invalidate(reg);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                sky = new Bitmap("sky.bmp");
+                plane = new Bitmap("plane.bmp");
+                this.BackgroundImage = new Bitmap("sky.bmp");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.ToString(), "Полет", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+                return;
+            }
+
+            plane.MakeTransparent();
+
+            this.ClientSize = new Size(new Point(BackgroundImage.Width,
+                BackgroundImage.Height));
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+
+            g = Graphics.FromImage(BackgroundImage);
+
+            rnd = new Random();
+
+            rct.X = -40;
+            rct.Y = 20 + rnd.Next(20);
+
+            rct.Width = plane.Width;
+            rct.Height = plane.Height;
+
+            dx = 2;
+
+            timer1.Interval = 20;
+            timer1.Enabled = true;
+        }
+    }
+}
